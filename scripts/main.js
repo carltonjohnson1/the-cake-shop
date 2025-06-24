6150db92141322cc43a7f3fbf4e852baa8c15064
carltonjohnson1 <carlton@carltonjohnson.com>
2025-06-24 22:24:49 +0100

    First upload

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+
+});
+
+
+// Close menu button when clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav linkis clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click())
+});
+
+// Initializing Swiper JS
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+// RESPONSIVE BREAKPOINTS  
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+
+  }
+
+});
